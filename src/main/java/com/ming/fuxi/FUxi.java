8e6414aed97a443d190b3c3ee3d997b198ec9b1b
13367073632 <108913210+13367073632@users.noreply.github.com>
2022-07-08 17:43:45 +0800

    pull commit

--- src/main/java/com/ming/fuxi/FUxi.java
+++ src/main/java/com/ming/fuxi/FUxi.java
@@ -11,6 +11,7 @@ public class FUxi {
         System.out.println("修改了代码");
         System.out.println("1111111111111111111111111111111");
         System.out.println("2222222222222222222222222222222");
+         System.out.println("从GitHub上修改了代码");
         return  "hello"+name;
     }
 
