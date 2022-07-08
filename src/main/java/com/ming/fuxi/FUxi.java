6a0419cbac3d3ee0f2dde8a49ea720b39223da8d
github登录 <2641674798@qq.com>
2022-07-08 16:28:21 +0800

    学习github

---
+++ src/main/java/com/ming/fuxi/FUxi.java
@@ -0,0 +1,11 @@
+package com.ming.fuxi;
+
+public class FUxi {
+
+    public   String h(String name){
+        System.out.println("嗯嗯");
+        return  "hello"+name;
+    }
+
+
+}
---
+++ src/test/java/Study.java
@@ -0,0 +1,9 @@
+import com.ming.fuxi.FUxi;
+
+public class Study {
+    public static void main(String[] args) {
+        FUxi fUxi = new FUxi();
+        String v = fUxi.h("小明");
+        System.out.println(v);
+    }
+}
