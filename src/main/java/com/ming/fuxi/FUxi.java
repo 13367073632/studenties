4ca31a7b0bb2bcb317831e3633e1d8821199d47d
github登录 <2641674798@qq.com>
2022-07-08 16:49:23 +0800

    学习分支合并

--- src/main/java/com/ming/fuxi/FUxi.java
+++ src/main/java/com/ming/fuxi/FUxi.java
@@ -5,6 +5,7 @@ public class FUxi {
     public   String h(String name){
         System.out.println("嗯嗯");
         System.out.println("哈哈");
+        System.out.println("branch01");
         return  "hello"+name;
     }
 
